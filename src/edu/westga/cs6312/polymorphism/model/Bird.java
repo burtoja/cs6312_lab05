e1b81dcccc573fe62114626cb5bc7e837cec52b7
J. Allen Burton <jburton6@my.westga.edu>
2020-02-01 06:32:35 -0500

    Created Bird subclass of Animal with constructor and getMovement override.

---
+++ src/edu/westga/cs6312/polymorphism/model/Bird.java
@@ -0,0 +1,49 @@
+package edu.westga.cs6312.polymorphism.model;
+
+/**
+ * Abstract subclass of Animal that creates Animals objects of type bird
+ * 
+ * @author J. Allen Burton
+ * @version Feb 1, 2020
+ *
+ */
+public abstract class Bird extends Animal{
+
+	/**
+	 * 
+	 * Constructor for Bird class which then constructs an Animal object of this kind with covering of feathers
+	 *
+	 * @param kind	type of Bird
+	 *
+	 * Precondition:
+	 *
+	 * Postcondition:	Animal object with type kind and covering of feathers
+	 */
+	public Bird(String kind) {
+		super(kind, "feathers");
+	}
+	
+	/**
+	 * This method is used to get a statement describing how the animal is moving
+	 * 
+	 * @param isMovingFast	true if moving fast and false if moving slow
+	 * 
+	 * @return	statement describing how the Animal is moving
+	 *
+	 * Precondition:		isMovingFast is boolean
+	 *
+	 * Postcondition:
+	 */
+	@Override
+	public String getMovement(boolean isMovingFast) {
+		String movementDescription;
+		if (isMovingFast == true) {
+			movementDescription = "I fly";
+		} else if (isMovingFast == false) {
+			movementDescription = "I walk on two legs";
+		} else {
+			throw new IllegalArgumentException("Invalid parameter provided for isMovingFast");
+		}
+		return movementDescription;
+	}
+}

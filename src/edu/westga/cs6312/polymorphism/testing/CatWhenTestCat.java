7148ef1cdd17fdcfbb8ef506fb9f01fe027e27f5
J. Allen Burton <jburton6@my.westga.edu>
2020-02-01 08:06:16 -0500

    Completed testing of Cat class

---
+++ src/edu/westga/cs6312/polymorphism/testing/CatWhenTestCat.java
@@ -0,0 +1,58 @@
+package edu.westga.cs6312.polymorphism.testing;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import edu.westga.cs6312.polymorphism.model.Cat;
+
+class CatWhenTestCat {
+
+	/**
+	 * Test Cat constructor and Animal toString method.  
+	 * The following values are expected:
+	 * kind = cat
+	 * covering = hair
+	 */
+	@Test
+	void testCreateNewCatExpectKindIsCatCoveringIsHair() {
+		Cat theCat = new Cat();
+		String report = theCat.toString();
+		assertEquals("Kind: cat -- Covering: hair", report);
+	}
+	
+	/**
+	 * Test Cat getSound method.  
+	 * The following values are expected:
+	 * meow
+	 */
+	@Test
+	void testCatGetSoundExpectMeow() {
+		Cat theCat = new Cat();
+		String report = theCat.getSound();
+		assertEquals("Meow", report);
+	}
+	
+	/**
+	 * Test Cat getMovement method (FAST).
+	 * If moving fast is true, expect "I run on four legs"
+	 */
+	@Test
+	void testCatGetMovementWhenFastExpectRunOn4() {
+		Cat theCat = new Cat();
+		String report = theCat.getMovement(true);
+		assertEquals("I run on four legs", report);
+	}
+	
+	/**
+	 * Test Cat getMovement method (SLOW).
+	 * If moving fast is false, expect "I walk on four legs"
+	 */
+	@Test
+	void testCatGetMovementWhenSlowExpectWalkOn4() {
+		Cat theCat = new Cat();
+		String report = theCat.getMovement(false);
+		assertEquals("I walk on four legs", report);
+	}
+
+}
